09af33855b8edb492574e0f899ce02e223e65fef
Pablo Alvarado Umaña <pablo.alvarado.umana@est.una.ac.cr>
2026-03-18 23:34:51 -0600

    fix: main route is contact

--- App2/routes/web.php
+++ App2/routes/web.php
@@ -4,14 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('contacto', function () {
     return view('contact');
 });
 
-Route::post('contacto', function (Request $request) {
+Route::post('/', function (Request $request) {
     $data = $request->only(['name', 'phone', 'address', 'email', 'marital_status']);
     return view('contact_info', ['data' => $data]);
 });
